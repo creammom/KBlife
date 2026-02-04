ca8a6fe09168cccb1337e633b35c06d21a66239c
creammom <creammom22@gmail.com>
2026-02-05 00:51:58 +0900

    섹션1 완성

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
 // 헤더
 $(function () {
-    $(".pc_header .menu_box > ul > li > a").on("mouseenter", function () {
-        $(".pc_header .menu_box a").removeClass("active");
-        $(".pc_header .sub_menu_wrap").hide();
-        $(this).addClass("active");
-        $(this).siblings(".sub_menu_wrap").show();
-    });
+  $(".pc_header .menu_box > ul > li > a").on("mouseenter", function () {
+    $(".pc_header .menu_box a").removeClass("active");
+    $(this).addClass("active");
+  });
 
-    $(".pc_header .menu_box > ul").on("mouseleave", function () {
-        $(".pc_header .menu_box a").removeClass("active");
-        $(".pc_header .sub_menu_wrap").hide();
-    });
+  $(".pc_header .menu_box > ul").on("mouseleave", function () {
+    $(".pc_header .menu_box a").removeClass("active");
+  });
+});
+
+
+// 스와이퍼
+
+const swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+const btn = document.querySelector(".swiper-toggle");
+btn.classList.add("pause");
+
+btn.addEventListener("click", () => {
+  if (swiper.autoplay.running) {
+    swiper.autoplay.stop();
+    btn.classList.replace("pause", "play");
+  } else {
+    swiper.autoplay.start();
+    btn.classList.replace("play", "pause");
+  }
 });
